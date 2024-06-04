a6733a959ecb332bdd9e000210944872c9a0a2f2
-h <debroy2021@gmail.com>
2024-06-03 21:50:11 -0500

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/anikaroy1204.github.io/",
+  base: "/anikaroy1204.github.io",
   plugins: [react()],
 })
