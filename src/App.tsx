ce612c72836bb51a2b7b6e874450e35bbbf50ed6
-h <debroy2021@gmail.com>
2024-06-04 11:27:45 -0500

    Flashing Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import Button from "./components/Button";
 
-function App() {
+const App = () => {
   const [alert, setAlert] = useState(false);
   return (
-    <>
-      <div>
-        <Button color="primary" onClick={() => setAlert(true)}>
-          Gundu
-        </Button>
-        {alert && <h1>Pontu</h1>}
-      </div>
-    </>
+    <div>
+      <Button onClick={() => setAlert(true)}>Gundu</Button>
+      {alert && <h1>Pontu</h1>}
+    </div>
   );
-}
+};
 
 export default App;
