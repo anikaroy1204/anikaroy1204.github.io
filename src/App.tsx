406690bafd803be9645ab8f501d46532a8f65809
-h <debroy2021@gmail.com>
2026-04-12 22:26:12 -0500

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import myimg from "./assets/images/image.png";
 
 const App = () => {
   const [alert, setAlert] = useState(false);
   return (
-    <div
-      className="d-flex align-items-center justify-content-center"
-      style={{ height: window.innerHeight }}
-    >
-      <Button onClick={() => setAlert(true)}>Didi is </Button>
-      {alert && <h1> Gundu Pontu</h1>}
-    </div>
+    <body>
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{ height: window.innerHeight }}
+      >
+        <Button onClick={() => setAlert(true)}>Didi is </Button>
+        {alert && <h1> Gundu Pontu</h1>}
+        <img src={myimg} alt="description" />
+      </div>
+    </body>
   );
 };
 
